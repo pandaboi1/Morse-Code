2ca40ba582e580fb9029b289af4f9bd2044e33b9
Edwin Serna <sernaedwin0@gmail.com>
2024-03-24 15:21:54 -0500

    Added mc_lib.py to store libraries

--- src/main.py
+++ src/main.py
@@ -3,38 +3,20 @@ Created By: Edwin Serna
 Date Create: 03/24/2024
 Date Edited: 03/24/2024 
 '''
-morse_code = { 
-    'A' : '.-',    'B' : '-...',  'C' : '-.-.', 
-    'D' : '-..',   'E' : '.',     'F' : '..-.', 
-    'G' : '--.',   'H' : '....',  'I' : '..', 
-    'J' : '.---',  'K' : '-.-',   'L' : '.-..', 
-    'M' : '--',    'N' : '-.',    'O' : '---', 
-    'P' : '.--.',  'Q' : '--.-',  'R' : '.-.', 
-    'S' : '...',   'T' : '-',     'U' : '..-', 
-    'V' : '...-',  'W' : '.--',   'X' : '-..-', 
-    'Y' : '-.--',  'Z' : '--..',
-    
-    '1' : '.----', '2' : '..---',  '3' : '...--',
-    '4' : '....-', '5' : '.....',  '6' : '-....',
-    '7' : '--...', '8' : '---..',  '9' : '----.',
-    '0' : '-----', 
-    
-    ',' : '--..--', '.' : '.-.-.-', '?' : '..--..', 
-    '/' : '-..-.',  '-' : '-....-',  
-    '(' : '-.--.',  ')' : '-.--.-' }
+import mc_lib
 
 def encription(inputedMessage):
     encrypted = ''
     for letter in inputedMessage:
-        if letter != morse_code[letter]:
-            print("\nCharacter", inputedMessage, "is not valid!")
-            break
         if letter != ' ':
-            encrypted += morse_code[letter]
+            encrypted += mc_lib.morse_code[letter]
         else:
             encrypted += ' '
     return encrypted    
 
 def main():
-    message = input()
-    print(encription(message.upper()))
+        message = input()
+        print(encription(message.upper()))
+        
+if __name__ == '__main__':
+ main()
