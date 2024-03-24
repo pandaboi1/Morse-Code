45dbb8410809a03cfac79cdba50e99d0697c00c0
Edwin Serna <sernaedwin0@gmail.com>
2024-03-24 16:02:32 -0500

    Added validate function and am giving an option to retype input

--- src/main.py
+++ src/main.py
@@ -5,18 +5,31 @@ Date Edited: 03/24/2024
 '''
 import mc_lib
 
-def encription(inputedMessage):
-    encrypted = ''
-    for letter in inputedMessage:
-        if letter != ' ':
-            encrypted += mc_lib.morse_code[letter]
-        else:
-            encrypted += ' '
-    return encrypted    
+# Validates the inputted message to make sure it can be encripted
+def Validate(inputMessage):
+    # Loops through inputted message one letter at a time to validate
+    for letter in inputMessage: 
+        x = 1
+    return True
+
+
+# Encrypts the inputted message from Morse Code dictionary
+def Encript(inputMessage):
+    encryptedMessage = ''
+    
+    # Loops through inputted message one letter at a time
+    for letter in inputMessage: 
+        # Checks key value pair from morse code dictionary 
+        encryptedMessage += mc_lib.morse_code[letter]             
+    return encryptedMessage   
 
 def main():
+    valid = False
+    while not valid:
         message = input()
-        print(encription(message.upper()))
+        valid = Validate(message)
+        
+    print(Encript(message))
         
 if __name__ == '__main__':
  main()
